ddae7f23f9fceeb9f5fb921c81ba8584a4991e1c
Algernonliang <132828750+Algernonliang@users.noreply.github.com>
2023-05-25 18:09:57 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+
+# markdown
+st.markdown('Streamlit Demo')
+
+# 设置网页标题
+st.title('一个傻瓜式构建可视化 web的 Python 神器 -- streamlit')
+
+# 展示一级标题
+st.header('1. 安装')
+
+st.text('和安装其他包一样，安装 streamlit 非常简单，一条命令即可')
+code1 = '''pip3 install streamlit'''
+st.code(code1, language='bash')
+
+
+# 展示一级标题
+st.header('2. 使用')
+
+# 展示二级标题
+st.subheader('2.1 生成 Markdown 文档')
+
+# 纯文本
+st.text('导入 streamlit 后，就可以直接使用 st.markdown() 初始化')
+
+# 展示代码，有高亮效果
+code2 = '''import streamlit as st
+st.markdown('Streamlit Demo')'''
+st.code(code2, language='python')
+
+st.caption("This is written small caption text")
+
+st.metric("My metric", 42, 2)
+
+import streamlit as st
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
+
+   import streamlit as st
+import pandas as pd
+import numpy as np
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
